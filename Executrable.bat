ecd2208a27229459e3f24e5848e1d283f946160c
rammohan4274 <rammohan4274@gmail.com>
2019-02-15 16:59:39 +0530

    RemoveFile1

--- Executrable.bat
+++
@@ -1,5 +0,0 @@
-set projectPath=C:\Users\RAMMOHAN\eclipse-workspace\JenkinsTestProjectBatch
-cd\
-cd %projectPath%
-set classpath=%projectPath%\bin;%projectPath%\lib\*;
-java org.testng.TestNG ExecteTestNG.xml
